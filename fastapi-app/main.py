c261ad053173dea1d60db1ca0135d40c28837cba
tjdus <dusml0277@gmail.com>
2025-11-10 21:25:33 +0900

    feat: Add prometheus

--- fastapi-app/main.py
+++ fastapi-app/main.py
@@ -3,10 +3,14 @@ from fastapi.responses import HTMLResponse
 from pydantic import BaseModel
 import json
 import os
+import os
+from prometheus_fastapi_instrumentator import Instrumentator
 
 from pydantic.v1 import Field
 
 app = FastAPI()
+
+Instrumentator().instrument(app).expose(app, endpoint="/metrics")
 
 # To-Do 항목 모델
 class TodoItem(BaseModel):
